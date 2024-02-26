e1046f411700ca14e830fe0b3a3f54a650f35cc3
chincapie <chincapie@intergrupo.com>
2024-02-26 10:27:24 -0500

    prueba pws 6

--- testParameters.ps1
+++ testParameters.ps1
@@ -1,9 +1,15 @@
-﻿$ServiceName = $args[0]
-$ServicePath = $args[1]
-$ServiceDescription = $args[2]
+﻿# $ServiceName = $args[0]
+# $ServicePath = $args[1]
+# $ServiceDescription = $args[2]
 
 write-host "There are a total of $($args.count) arguments"
 
-Write-Output $ServiceName
-Write-Output $ServicePath
-Write-Output $ServiceDescription
+for ($i = 0; $i -lt $args.Length; $i++)
+{
+    # Output the current item
+    Write-Host $args[$i]
+}
+
+# Write-Output $ServiceName
+# Write-Output $ServicePath
+# Write-Output $ServiceDescription
