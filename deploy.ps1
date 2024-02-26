a0f44c20029ac22e67ed0b11b63d5e77f234433d
chincapie <chincapie@intergrupo.com>
2024-02-26 11:06:03 -0500

    prueba pws 7

--- deploy.ps1
+++ deploy.ps1
@@ -1,20 +1,34 @@
-Set-Item wsman:\localhost\client\TrustedHosts -Value ${{ env.IP_DEPLOY }} -Force
-Write-Output ${{ github.workspace }}
-Get-ChildItem -Path ${{ github.workspace }}
-$User = ${{ env.IP_DEPLOY }} + "\" + ${{ env.USERNAME_DEPLOY }}
-$PWord = ConvertTo-SecureString -String ${{ env.PASSWORD_DEPLOY }} -AsPlainText -Force
+$IP_DEPLOY = $args[0]
+$WORKSPACE = $args[1]
+$USERNAME_DEPLOY = $args[2]
+$BASE = $args[3]
+$DIRECTORYDESTINY = $args[4]
+
+write-host "There are a total of $($args.count) arguments"
+
+for ($i = 0; $i -lt $args.Length; $i++)
+{
+    # Output the current item
+    Write-Host $args[$i]
+}
+
+Set-Item wsman:\localhost\client\TrustedHosts -Value $IP_DEPLOY -Force
+Write-Output $WORKSPACE
+Get-ChildItem -Path $WORKSPACE
+$User = "$($IP_DEPLOY)\$($USERNAME_DEPLOY)"
+$PWord = ConvertTo-SecureString -String $IP_DEPLOY -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-$Session = New-PSSession -ComputerName ${{ env.IP_DEPLOY }} -Credential $Credential
+$Session = New-PSSession -ComputerName $IP_DEPLOY -Credential $Credential
 $ConfirmPreference = 'None'
 $ServiceDescription = "Servicio Windows rca_gtm_synergy_suite_ServBoHExtInventarioTrasladoSS"
-$Origin = ${{ github.workspace }}
-$Base = ${{ env.BASE }}
-$DirectoryDestiny = ${{ env.DIRECTORYDESTINY }}
-$Destination = "$($Base)\$($DirectoryDestiny)\"
+$Origin = $WORKSPACE
+$DirectoryDestiny = $DIRECTORYDESTINY
+$Destination = "$($BASE)\$($DirectoryDestiny)\"
 Write-Output "Destino:" + $Destination
 $ServicePath = "$($Destination)\ServBoHExtInventarioTrasladoSS.exe"
 $ServiceName = "Service_$($Destination)".Replace("\","_").Replace(":","").TrimEnd("_")
 Write-Output "ServicePath: " + $ServicePath
+Write-Output "ServicePath: " + $ServiceName
 get-Item WSMan:\localhost\Client\TrustedHosts
 Test-NetConnection 172.191.231.77 -Port 5985
 
